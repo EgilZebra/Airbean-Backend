fbe9e6cc67dd24b880fd1a782bc194b41c72d717
Egil Ramsten <egil.ramsten@hotmail.com>
2024-04-16 13:42:06 +0200

    Update to index and strukture of files

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const express = require("express");
-const Datastore = require("nedb-promises");
-const { v4: uuidv4 } = require("uuid");
 
 //modules
 const about = require("./about.js");
-const menu = require("./menu.js");
+const { menu } = require("./menu.js");
 const user = require("./user.js");
 const order = require("./order.js");
 
@@ -22,41 +20,3 @@ const URL = "127.0.0.1";
 const server = app.listen(PORT, URL, () => {
   console.log("Hej jag är en server!");
 });
-
-// // Lägga en Order
-// app.post("/order", async (req, res) => {
-//   const { userid, cart } = req.body;
-
-//   let order = 0;
-//   let check = [];
-//   const mymenu = await menu.find({});
-
-//   for (const item of cart) {
-//     const menuItem = mymenu.find(
-//       (menuItem) =>
-//         menuItem.title === item.item.title && menuItem.price === item.item.price
-//     );
-//     check.push(menuItem);
-//     console.log("menuitem", menuItem);
-//   }
-
-//   try {
-//     if (check.every(({ element }) => element !== "undefined")) {
-//       order = {
-//         ordernumber: uuidv4(),
-//         user: userid ? userid : "Gäst",
-//         eta: Math.floor(Math.random() * 30),
-//         cart: cart,
-//       };
-
-//       await orders.insert(order);
-//       res.status(200).json({
-//         message: "Order sent!",
-//         eta: order.eta,
-//         ordernummer: order.ordernumber,
-//       });
-//     }
-//   } catch (error) {
-//     res.status(400).send("Order misslyckades!");
-//   }
-// });
--- order.js
+++ order.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const Datastore = require("nedb-promises");
 const { v4: uuidv4 } = require("uuid");
-
 const { menuData } = require("./menu.js");
 
 const order = express();
